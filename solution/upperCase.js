3904cbbcf725cfbeb21dc2b64bb7feed7e3c1c1f
lush81_Shapovalenko <lush@i.ua>
2015-02-21 19:44:05 +0200

    tests

---
+++ solution/upperCase.js
@@ -0,0 +1,7 @@
+
+function upperCase(input){
+
+return input.toUpperCase();
+}
+
+module.exports = upperCase;
---
+++ test/upperCaseSpec.js
@@ -0,0 +1,12 @@
+var upperCase = require('../solution/upperCase');
+var assert = require("assert");
+
+ describe('upperCase', function(){
+  
+    it('should return upperCase', function(){
+      assert.equal(upperCase('test'), 'TEST');
+      
+    });
+
+});
+
